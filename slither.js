f5dfc91f3b41b6f0d115bb80316f08c9c5dab056
Vighnesh Radhakrishnan <vighnesh7radhakrishnan@gmail.com>
2025-01-03 00:33:59 +0530

    snake direction change control added

--- slither.js
+++ slither.js
@@ -5,6 +5,7 @@ const SNAKE_COLOR = "lightgreen";
 const SNAKE_BORDER_COLOR = "darkgreen";
 
 let snake = [
+  { x: 150, y: 140 },
   { x: 150, y: 150 },
   { x: 140, y: 150 },
   { x: 130, y: 150 },
@@ -13,9 +14,9 @@ let snake = [
 ];
 
 // Horizontal velocity
-let dx = 10;
+let dx = 0;
 // Vertical velocity
-let dy = 0;
+let dy = -10;
 
 // Get the canvas element
 var gameCanvas = document.getElementById("gameCanvas");
@@ -34,6 +35,38 @@ function clearCanvas() {
   ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
   ctx.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
 }
+
+function changeDirection(event) {
+  const LEFT_KEY = 37;
+  const RIGHT_KEY = 39;
+  const UP_KEY = 38;
+  const DOWN_KEY = 40;
+  const keyPressed = event.keyCode;
+  const goingUp = dy === -10;
+  const goingDown = dy === 10;
+  const goingRight = dx === 10;
+  const goingLeft = dx === -10;
+  if (keyPressed === LEFT_KEY && !goingRight) {
+    dx = -10;
+    dy = 0;
+  }
+  if (keyPressed === UP_KEY && !goingDown) {
+    dx = 0;
+    dy = -10;
+  }
+  if (keyPressed === RIGHT_KEY && !goingLeft) {
+    dx = 10;
+    dy = 0;
+  }
+  if (keyPressed === DOWN_KEY && !goingUp) {
+    dx = 0;
+    dy = 10;
+  }
+}
+
+// listen to key press, keyDown is used since we want the action to be done
+// as the key is pressed and not when released
+document.addEventListener("keydown", changeDirection);
 
 function advanceSnake() {
   const head = { x: snake[0].x + dx, y: snake[0].y + dy };
@@ -56,6 +89,8 @@ function drawSnakePart(snakePart) {
   ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
 }
 
+main();
+
 function main() {
   setTimeout(function onTick() {
     clearCanvas();
@@ -64,3 +99,5 @@ function main() {
     main();
   }, 100);
 }
+
+drawSnake();
